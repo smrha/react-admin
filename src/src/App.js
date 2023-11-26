5cd578c3a44611c5bec4bced8b6e2ae4e06ccb65
smrha <lord.smrha@gmail.com>
2023-11-26 15:12:21 +0330

    Initial project

---
+++ src/src/App.js
@@ -0,0 +1,17 @@
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import {ColorModeContext, useMode } from "./theme"
+
+function App() {
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            
+          </div>
+  );
+}
+
+export default App;
